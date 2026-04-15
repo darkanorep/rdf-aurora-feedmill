f071a8ce0bc7c8d9a976acb938e49eda777a861d
jperona <jeromeperona@yahoo.com>
2026-04-15 13:53:05 +0800

    Add Filterable trait to Wastage model; set default filters for wastage management

--- app/Models/Wastage.php
+++ app/Models/Wastage.php
@@ -2,12 +2,15 @@
 
 namespace App\Models;
 
+use App\Filters\WastageFilter;
+use Essa\APIToolKit\Filters\Filterable;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Wastage extends Model
 {
-    use SoftDeletes;
+    use SoftDeletes, Filterable;
 
     protected $guarded = [];
+    protected $default_filters = WastageFilter::class;
 }
