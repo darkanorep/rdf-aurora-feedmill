6967fa55b218930affea304d1e4519c0ebc61dd9
jperona <jeromeperona@yahoo.com>
2026-04-27 08:52:20 +0800

    Add 'id' field to Score, Unit, and Wastage resources for improved data representation

--- app/Http/Resources/ScoreResource.php
+++ app/Http/Resources/ScoreResource.php
@@ -15,6 +15,7 @@ class ScoreResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
+            'id' => $this->id,
             'score' => $this->score,
             'rating' => $this->rating
         ];
